243465abfce978bfad75f4d85648365684b108e9
Karan N <karan820416@gmail.com>
2026-09-08 15:58:28 +0530

    added upi and qr function

---
+++ app/upi.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+from urllib.parse import urlencode
+
+import qrcode
+
+def create_upi_uri(upi_id : str, name: str, amount: Decimal, note: str | None = None) -> str:
+    params = {
+        "pa": upi_id,
+        "pn":name,
+        "am":f"{amount:.2f}",
+        "cu":"INR",
+    }
+
+
+    if note:
+        params["tn"] = note
+
+    return "upi://pay?" + urlencode(params)
+
+
+def create_qr_code(upi_uri:str) -> bytes:
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        box_size=10,
+        border=4,   
+    )
+
